72017c53e87baf95904bea06199fa8fd7ab0ec53
Joshua Fernandes <fernandes.josh@gmail.com>
2022-12-22 12:31:51 -0500

    feat: added table to display media

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,11 +15,38 @@ const Home: NextPage = () => {
         <h1 className="text-center text-2xl font-bold">
           Social Delinquents Media Club
         </h1>
-        <AddMedia />
-        {data &&
-          data.map((media, index) => {
-            return <div key={index}>{media.name}</div>;
-          })}
+        <div className="flex flex-row justify-center space-x-5">
+          <div className="flex w-1/3 max-w-md grow-0 flex-col">
+            <AddMedia />
+          </div>
+          {data && (
+            <div className="w-2/3">
+              <div className="grid grid-cols-5 items-center p-1 text-lg font-bold">
+                <span>Name</span>
+                <span>Genre</span>
+                <span>Watched</span>
+                <span>People Watched</span>
+                <span>Media Type</span>
+              </div>
+              <div className="space-y-3">
+                {data.map((media, index) => {
+                  return (
+                    <div
+                      key={index}
+                      className="grid grid-cols-5 p-2 odd:bg-slate-900"
+                    >
+                      <span>{media.name}</span>
+                      <span>{media.genre}</span>
+                      <span>{media.watched ? "Yes" : "No"}</span>
+                      <span>{null}</span>
+                      <span>{media.type}</span>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          )}
+        </div>
       </main>
     </>
   );

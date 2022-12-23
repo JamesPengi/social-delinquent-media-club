8b7ec6c9a439c87d45b8b56b864d959d31fc708a
Joshua Fernandes <fernandes.josh@gmail.com>
2022-12-22 20:29:14 -0500

    refactor: changed styling

--- src/components/AddMedia.tsx
+++ src/components/AddMedia.tsx
@@ -26,12 +26,7 @@ const AddMedia: React.FC = () => {
       reset();
     },
   });
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm({
+  const { register, handleSubmit, reset } = useForm({
     resolver: zodResolver(schema),
   });
 
@@ -40,23 +35,52 @@ const AddMedia: React.FC = () => {
       onSubmit={handleSubmit(({ name, genre, mediaType }) => {
         mutation.mutate({ name, genre, mediaType });
       })}
-      className="flex flex-col space-y-3 rounded bg-slate-900 p-5"
+      className="flex flex-col space-y-7 rounded bg-slate-900 p-5"
     >
-      <div className="flex flex-row space-x-3">
-        <label
-          htmlFor="mediaType"
-          className="flex flex-col justify-center space-y-1"
-        >
-          <span className="font-bold">Media Type</span>
-          <select
-            id="mediaType"
-            className="p-0.5 dark:bg-slate-700"
-            {...register("mediaType")}
+      <div className="flex flex-col text-center">
+        <h3 className="text-2xl font-bold">Add Media</h3>
+        <span className="text-xs font-extrabold uppercase text-gray-400">
+          Add stuff to watch to the list!
+        </span>
+      </div>
+      <div className="space-y-5">
+        <div className="flex flex-row space-x-5">
+          <label
+            htmlFor="mediaType"
+            className="flex flex-col justify-center space-y-1"
           >
-            <option value="Movie">Movie</option>
-            <option value="TV">TV</option>
-          </select>
-        </label>
+            <span className="font-bold">Media Type</span>
+            <select
+              id="mediaType"
+              className="rounded p-0.5 py-1 dark:bg-slate-700"
+              {...register("mediaType")}
+            >
+              <option value="Movie">Movie</option>
+              <option value="TV">TV</option>
+            </select>
+          </label>
+
+          <label
+            htmlFor="genre"
+            className="flex flex-col justify-center space-y-1"
+          >
+            <span className="font-bold">Genre</span>
+            <select
+              id="mediaGenre"
+              className="rounded px-0.5 py-1 dark:bg-slate-700"
+              {...register("genre")}
+            >
+              <option value="Drama">Drama</option>
+              <option value="Comedy">Comedy</option>
+              <option value="Horror">Horror</option>
+              <option value="Animated">Animated</option>
+              <option value="Action">Action</option>
+              <option value="Adventure">Adventure</option>
+              <option value="Sadge">Sadge</option>
+              <option value="JapaneseAnimation">Japanese Animation</option>
+            </select>
+          </label>
+        </div>
 
         <label
           htmlFor="name"
@@ -65,29 +89,11 @@ const AddMedia: React.FC = () => {
           <span className="font-bold">Name</span>
           <input
             type="text"
-            className="border-b-2 border-black bg-transparent outline-none ring-0 transition-colors focus:border-sky-500 dark:border-white"
+            className="border-b-2 border-black bg-transparent py-1 outline-none ring-0 transition-colors focus:border-sky-500 dark:border-white"
             {...register("name")}
           />
         </label>
       </div>
-
-      <label htmlFor="genre" className="flex flex-col justify-center space-y-1">
-        <span className="font-bold">Genre</span>
-        <select
-          id="mediaGenre"
-          className="px-0.5 py-1 dark:bg-slate-700"
-          {...register("genre")}
-        >
-          <option value="Drama">Drama</option>
-          <option value="Comedy">Comedy</option>
-          <option value="Horror">Horror</option>
-          <option value="Animated">Animated</option>
-          <option value="Action">Action</option>
-          <option value="Adventure">Adventure</option>
-          <option value="Sadge">Sadge</option>
-          <option value="JapaneseAnimation">Japanese Animation</option>
-        </select>
-      </label>
 
       <div className="flex grow-0 items-center justify-center">
         <input
